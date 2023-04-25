6ca97184249ea51578c2757b277c94cb234d47cd
unknown <sgarciac01@hotmail.com>
2023-04-25 13:43:38 +0200

    VNF management added

--- client.py
+++ client.py
@@ -9,10 +9,33 @@ import logging
 import sys
 
 
+class VNF:
+    def __init__(self, json_data):
+        self.source = json_data['source']
+        self.target = json_data['target']
+        self.gpu = json_data['gpu']
+        self.ram = json_data['ram']
+        self.bw = json_data['bw']
+        self.rtt = json_data['rtt']
+        self.previous_node = json_data['previous_node']
+        self.current_node = json_data['current_node']
+        self.fec_linked = json_data['fec_linked']
+        self.user_id = json_data['user_id']
+
+
 logger = logging.getLogger('')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 logger.addHandler(logging.FileHandler('ue.log', mode='w', encoding='utf-8'))
 logger.addHandler(logging.StreamHandler(sys.stdout))
+
+best_mac = ""
+client_socket = socket.socket()
+connected = False
+fec_id = -1
+user_id = 1
+my_vnf = None
+previous_node = -1
+send_vnf = True
 
 
 def get_rssi():
@@ -25,6 +48,9 @@ def get_rssi():
                 handover(best_mac)
 
         time.sleep(2)
+
+
+rssi_thread = threading.Thread(target=get_rssi)
 
 
 def get_mac_to_connect():
@@ -58,19 +84,26 @@ def handover(mac):
 
 def disconnect(starting):
     global connected
-    if not starting:
-        message = json.dumps(dict(type="bye"))  # take input
-        client_socket.send(message.encode())  # send message
-        client_socket.recv(1024).decode()  # receive response
-        kill_thread(server_thread.ident)
-        server_thread.join()
-    process_disconnect = subprocess.Popen(
-        'netsh wlan disconnect',
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
-    process_disconnect.communicate()
-    connected = False
+    global previous_node
+    try:
+        if not starting and connected:
+            message = json.dumps(dict(type="bye"))  # take input
+            client_socket.send(message.encode())  # send message
+            client_socket.recv(1024).decode()  # receive response
+            kill_thread(server_thread.ident)
+            server_thread.join()
+        previous_node = fec_id
+        process_disconnect = subprocess.Popen(
+            'netsh wlan disconnect',
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        process_disconnect.communicate()
+        connected = False
+    except ConnectionResetError:
+        logger.critical('[!] Trying to reuse killed connection!')
+    except Exception as e:
+        logger.exception(e)
 
 
 def connect(mac):
@@ -102,6 +135,9 @@ def connect(mac):
 
 def server_conn():
     global client_socket
+    global fec_id
+    global my_vnf
+    global send_vnf
     host = '10.0.0.1'
     port = 5010  # socket server port number
     try:
@@ -117,10 +153,54 @@ def server_conn():
 
         client_socket.send(message.encode())  # send message
         data = client_socket.recv(1024).decode()  # receive response
+        json_data = json.loads(data)
+        if json_data['res'] == 200:
+            logger.info('[I] Response from FEC: ' + str(json_data))
+            fec_id = json_data['id']
+        else:
+            logger.error('[!] Error ' + json_data['res'] + ' when authenticating to FEC!')
 
-        logger.info('[I] Received from server: ' + data)  # show in terminal
+        if send_vnf:
+            input('[*] Press enter to send a VNF...')
 
-        # message = input(" -> ")  # again take input
+            if my_vnf is None:
+                if fec_id == 1:
+                    target = 2
+                else:
+                    target = 1
+                my_vnf = VNF(dict(source=fec_id, target=target, gpu=512, ram=5, bw=250, rtt=0.5, previous_node=-1,
+                                  current_node=fec_id, fec_linked=fec_id, user_id=user_id))
+            else:
+                my_vnf.previous_node = previous_node
+                my_vnf.current_node = fec_id
+            message = json.dumps(dict(type="vnf", data=my_vnf.__dict__))  # take input
+            client_socket.send(message.encode())  # send message
+            data = client_socket.recv(1024).decode()  # receive response
+            json_data = json.loads(data)
+            if json_data['res'] == 200:
+                # MAKE CAR MOVE TOWARDS ACTION DIRECTION
+                logger.info('[I] Response from server: ' + str(json_data['action']))  # NOT IMPLEMENTED!
+                if json_data['action'] == 'e':
+                    logger.info('[I] Car reached target!')
+                    key_in = input('[*] Press enter to send a new VNF... type "q" for quitting')
+                    if key_in != 'q':
+                        if fec_id == 1:
+                            target = 2
+                        else:
+                            target = 1
+                        my_vnf = VNF(
+                            dict(source=fec_id, target=target, gpu=512, ram=5, bw=250, rtt=0.5, previous_node=-1,
+                                 current_node=fec_id, fec_linked=fec_id, user_id=user_id))
+                        message = json.dumps(dict(type="vnf", data=my_vnf.__dict__))  # take input
+                        client_socket.send(message.encode())  # send message
+                        data = client_socket.recv(1024).decode()  # receive response
+                        json_data = json.loads(data)
+                        if json_data['res'] == 200:
+                            logger.info('[I] Response from server: ' + str(json_data['action']))  # NOT IMPLEMENTED!
+                        else:
+                            logger.error('[!] Error ' + json_data['res'] + ' when sending VNF to FEC!')
+            else:
+                logger.error('[!] Error ' + json_data['res'] + ' when sending VNF to FEC!')
         while True:
             time.sleep(1)
 
@@ -130,6 +210,11 @@ def server_conn():
         message = json.dumps(dict(type="bye"))  # take input
         client_socket.send(message.encode())  # send message
         client_socket.close()  # close the connection
+    except Exception as e:
+        logger.exception(e)
+
+
+server_thread = threading.Thread(target=server_conn)
 
 
 def kill_process(process_kill):
@@ -147,15 +232,8 @@ def kill_thread(thread_id):
             ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
             raise SystemError("PyThreadState_SetAsyncExc failed")
         logger.info("[I] Successfully killed thread " + str(thread_id))
-    except TypeError:
-        pass
-
-
-best_mac = ""
-client_socket = socket.socket()
-rssi_thread = threading.Thread(target=get_rssi)
-server_thread = threading.Thread(target=server_conn)
-connected = False
+    except Exception as e:
+        logger.exception(e)
 
 
 def main():
@@ -178,19 +256,25 @@ def main():
         rssi_thread.daemon = True
         rssi_thread.start()
 
-        input('[*] Press enter to stop...')
+        if send_vnf:
+            while True:
+                time.sleep(1)
 
-        logger.info('[!] Ending...')
-        kill_thread(rssi_thread.ident)
-        rssi_thread.join()
-        disconnect(False)
+        else:
+            input('[*] Press enter to stop...')
+
+            logger.info('[!] Ending...')
+            kill_thread(rssi_thread.ident)
+            rssi_thread.join()
+            disconnect(False)
     except KeyboardInterrupt:
         logger.info('[!] Ending...')
         disconnect(False)
-        kill_thread(rssi_thread.ident)
-        rssi_thread.join()
-    except RuntimeError:
-        pass
+        if connected:
+            kill_thread(rssi_thread.ident)
+            rssi_thread.join()
+    except Exception as e:
+        logger.exception(e)
 
 
 if __name__ == '__main__':
